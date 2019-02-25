d247744d16f0bdb29e3f68518fc1ece5512e3afe
Suren <mnatsakanovsuren@gmail.com>
2019-02-25 18:07:32 +0200

    add some styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 class Form extends Component {
     render() {
         return (
-            <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="Enter city"/>
-                <button>Show weather</button>
-            </form>
+            <div className="FormSide">
+                <form onSubmit={this.props.getWeather}>
+                    <input type="text" name="city" placeholder="Enter city"/>
+                    <button>Show weather</button>
+                </form>
+            </div>
+
         );
     }
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class Weather extends Component {
     render() {
         return (
-            <div>
+            <div className="InfoSide">
                 { this.props.city &&
                     <div className="info-block">
                         <p><b>City: </b>{this.props.city}</p>
                         <p><b>Country: </b>{this.props.country}</p>
-                        <p><b>Temperature: </b>{this.props.temp}</p>
+                        <p><b>Temperature: </b>{this.props.temp} &deg;C</p>
                     </div>
                 }
             </div>
